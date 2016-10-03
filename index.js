a0306e3e5be7dee82c25c31281173b885cbef88d
lazaruslarue <lazaruslarue@gmail.com>
2016-10-03 09:39:28 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {Router, Route, hashHistory} from 'react-router'
-import App from './modules/App'
-import About from './modules/About'
-import Repos from './modules/Repos'
+// import {Router,  hashHistory} from 'react-router'
+// import App from './modules/App'
+// import About from './modules/About'
+// import Repos from './modules/Repos'
+// import Repo from './modules/Repo'
+// import Router from 'react-router/BrowserRouter'
 
+import { BrowserRouter, Link, Match } from 'react-router'
+render(  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
 
-render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/about" component={About}/>
-      <Route path="/repos" component={Repos}/>
-    </Route>
-  </Router>
-), document.getElementById('app'))
+      <hr/>
+
+    </div>
+  </BrowserRouter>, document.getElementById('app'))
