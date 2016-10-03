3c4f4ba0a0599da5678d910f0dea5ca18499e2d2
lazaruslarue <lazaruslarue@gmail.com>
2016-10-03 10:30:07 -0600

    cleanup

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import {Router,  hashHistory} from 'react-router'
-// import App from './modules/App'
-// import About from './modules/About'
-// import Repos from './modules/Repos'
-// import Repo from './modules/Repo'
-// import Router from 'react-router/BrowserRouter'
 
-import { BrowserRouter, Link, Match } from 'react-router'
-render(  <BrowserRouter>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
+import App from './modules/App'
 
-      <hr/>
 
-    </div>
-  </BrowserRouter>, document.getElementById('app'))
+render(  <App />, document.getElementById('app'))
